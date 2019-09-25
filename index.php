67d5aa15b8913a94d4127a0618856b470b902e78
bayufrd <55101246+bayufrd@users.noreply.github.com>
2019-09-25 17:51:40 +0700

    index.php redesign

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
     <head>
         
     <link rel="shortcut icon" href="./mockup/favicon.ico">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="posisi.css">
-        <title>Home</title>
+        <title>Jual Sapi | Home</title>
     </head>
     <body>
             <nav class="navbar navbar-light bg-light">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand" href="#"> -->  
                     <!--<img src="https://cdn.pixabay.com/photo/2016/06/28/17/26/cow-1485012_960_720.png" width="30" height="30" class="d-inline-block align-top" alt="">-->  
-                    <img src="./mockup/LOGO.png" width="30" height="30" class="d-inline-block align-top" alt="">
+  <!--                  <img src="./mockup/LOGO.png" width="30" height="30" class="d-inline-block align-top" alt="">
                       Jual Sapi
                     </a>
             </nav>
@@ -19,11 +19,86 @@
                 <a type="button" class="btn btn-secondary" href="login_page.php">Masuk</a>
                 <a type="button" class="btn btn-secondary" href="register_page.php">Daftar</a>
             </div>
-
+-->
 <!--
                 <img src="https://cdn.pixabay.com/photo/2016/06/28/17/26/cow-1485012_960_720.png" width="300" height="300">
-            -->
+            --><!--
                 <img src="./mockup/LOGO.png" width="320" height="300" >
             </a>                 
     </body>
+</html>
+-->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="shortcut icon" href="./mockup/favicon.ico">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Jual Sapi | Home
+    </title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md" style="height: 28px;">
+        <div class="container-fluid"><button class="navbar-toggler" data-toggle="collapse"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button></div>
+    </nav>
+    <div style="height: 44px;">
+        <nav class="navbar navbar-light navbar-expand-md navigation-clean" style="background-color: rgb(193, 182, 58);height: 62px;font-size: 15px;margin: -29px;width: 1469px;">
+            <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navcol-1">
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="./">HOME</a>
+                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">Penjualan</a><a class="dropdown-item" role="presentation" href="#">Profil</a><a class="dropdown-item" role="presentation" href="#">Menu 3</a>
+                            <div class="dropdown-divider" role="presentation"></div>
+                        </div>
+                    </li>
+                    <div class="row padMar">
+                        <div class="col padMar" style="padding-right: -27px;padding-left: 87px;min-width: 5px;height: 35px;margin-right: 3px;margin-top: -1px;margin-left: 15px;padding-bottom: 6px;max-width: 604px;width: 551px;">
+                            <div class="input-group">
+                                <div class="input-group-prepend"></div><input class="form-control autocomplete" type="text" placeholder="Search...">
+                                <div class="input-group-append"><button class="btn btn-warning" type="button" style="background-color: rgb(255,231,158);"><i class="fa fa-search"></i></button></div>
+                            </div>
+                        </div>
+                    </div><a href="about.html" style="font-size: 11px;padding: 11px;color: rgb(255,255,255);width: 85px;padding-right: -3px;padding-left: 242px;padding-top: 3px;margin: -3px;margin-top: -3px;height: 43px;">Tentang Kami</a>
+                    <ul class="nav navbar-nav ml-auto"
+                        style="height: 53px;">
+                        <li class="nav-item" role="presentation"></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="help.html" style="font-size: 11px;padding: 7px;color: rgb(255,255,255);width: 88px;">Pusat Bantuan</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="contact.html" style="font-size: 11px;padding: 7px;color: rgb(255,255,255);width: 88px;">Hubungi Kami</a><button class="btn btn-primary" type="button" style="background-color: rgb(188,151,94);height: 24px;font-size: 8px;">Masuk</button>
+                            <button
+                                class="btn btn-primary" type="button" style="background-color: rgb(188,151,94);height: 24px;font-size: 8px;">Daftar</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div></div><img style="min-width: -3px;max-width: 0px;"><img src="assets/img/LOGO.png" style="height: 231px;margin: 46px;"><img src="assets/img/JualSapi.png" style="height: 101px;margin: -320px;padding: 9px;margin-top: 33px;">
+    <div style="margin-bottom: 14px;margin-top: 104px;background-color: #d9d9d9;width: 1445px;min-width: -2px;min-height: -1px;padding: 42px;padding-top: 112px;padding-left: 50px;padding-right: 44px;padding-bottom: 144px;">
+        <ul class="nav nav-tabs" style="height: 45px;width: 1218px;margin: -4px;margin-bottom: 40px;min-height: 7px;margin-top: -75px;">
+            <li class="nav-item"><a class="nav-link active" role="tab" data-toggle="tab" href="#tab-1">Tab 1</a></li>
+            <li class="nav-item"><a class="nav-link" role="tab" data-toggle="tab" href="#tab-2">Tab 2</a></li>
+            <li class="nav-item"><a class="nav-link" role="tab" data-toggle="tab" href="#tab-3">Tab 3</a></li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane active" role="tabpanel" id="tab-1">
+                <p>Content for tab 1.</p>
+            </div>
+            <div class="tab-pane" role="tabpanel" id="tab-2">
+                <p>Content for tab 2.</p>
+            </div>
+            <div class="tab-pane" role="tabpanel" id="tab-3">
+                <p>Content for tab 3.</p>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
 </html>
